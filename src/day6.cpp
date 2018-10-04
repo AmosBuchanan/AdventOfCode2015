aeb0b57982a4c3af2e3c9bc00c03907a0909f60b
Amos <amos@amosbuchanan.net>
2018-10-03 18:04:56 -0700

    Formating change.

--- src/day6.cpp
+++ src/day6.cpp
@@ -10,26 +10,26 @@
 /**
    --- Day 6: Probably a Fire Hazard ---
 
-Because your neighbors keep defeating you in the holiday house
-decorating contest year after year, you've decided to deploy one
-million lights in a 1000x1000 grid.
+   Because your neighbors keep defeating you in the holiday house
+   decorating contest year after year, you've decided to deploy one
+   million lights in a 1000x1000 grid.
 
-Furthermore, because you've been especially nice this year, Santa has
-mailed you instructions on how to display the ideal lighting
-configuration.
+   Furthermore, because you've been especially nice this year, Santa has
+   mailed you instructions on how to display the ideal lighting
+   configuration.
 
-Lights in your grid are numbered from 0 to 999 in each direction; the
-lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The
-instructions include whether to turn on, turn off, or toggle various
-inclusive ranges given as coordinate pairs. Each coordinate pair
-represents opposite corners of a rectangle, inclusive; a coordinate
-pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3
-square. The lights all start turned off.
+   Lights in your grid are numbered from 0 to 999 in each direction; the
+   lights at each corner are at 0,0, 0,999, 999,999, and 999,0. The
+   instructions include whether to turn on, turn off, or toggle various
+   inclusive ranges given as coordinate pairs. Each coordinate pair
+   represents opposite corners of a rectangle, inclusive; a coordinate
+   pair like 0,0 through 2,2 therefore refers to 9 lights in a 3x3
+   square. The lights all start turned off.
 
-To defeat your neighbors this year, all you have to do is set up your
-lights by doing the instructions Santa sent you in order.
+   To defeat your neighbors this year, all you have to do is set up your
+   lights by doing the instructions Santa sent you in order.
 
-For example:
+   For example:
 
     turn on 0,0 through 999,999 would turn on (or leave on) every
     light.
@@ -41,8 +41,8 @@ For example:
     turn off 499,499 through 500,500 would turn off (or leave off) the
     middle four lights.
 
-After following the instructions, how many lights are lit?
-**/
+   After following the instructions, how many lights are lit?
+ **/
 
 
 #include <helpers.h>
@@ -53,133 +53,137 @@ After following the instructions, how many lights are lit?
 
 struct lightgrid
 {
-    uint8 *Grid;
+  uint8 *Grid;
 
-    int32 Rows;
-    int32 Cols;
-    size_t Size;
+  int32 Rows;
+  int32 Cols;
+  size_t Size;
 };
 
 enum operation
 {
-    OP_NOP,
-    OP_ON,
-    OP_OFF,
-    OP_TOGGLE
+  OP_NOP,
+  OP_ON,
+  OP_OFF,
+  OP_TOGGLE
 };
 
 void
 InitializeGrid(lightgrid *Grid)
 {
-    
-    uint64 *Pos = (uint64*) Grid->Grid;
-    uint8 *End = Grid->Grid + Grid->Size;
 
-    while((Pos+1) < (uint64 *)End)
+  uint64 *Pos = (uint64*) Grid->Grid;
+  uint8 *End = Grid->Grid + Grid->Size;
+
+  while((Pos + 1) < (uint64 *)End)
+  {
+    *Pos++ = 0;
+  }
+
+  if(Grid->Size % 8)
+  {
+    uint8 *LastBit = (uint8 *)Pos;
+
+    while(LastBit < End)
     {
-        *Pos++ = 0;
+      *LastBit++ = 0;
     }
-
-    if(Grid->Size % 8)
-    {
-        uint8 *LastBit = (uint8 *)Pos;
-
-        while(LastBit < End)
-        {
-            *LastBit++ = 0;
-        }
-    }
+  }
 
 }
 
 int32
 GetCoord(char **String)
 {
-    int32 Result = 0;
-    char num[4];
-    char *pos = *String;
-    int i = 0;
-    while((*pos >= '0') && (*pos <= '9'))
-    {
-        num[i++] = *pos++;
-    }
-    num[i] = '\0';
-    Result = atoi(num);
+  int32 Result = 0;
+  char num[4];
+  char *pos = *String;
+  int i = 0;
+  while((*pos >= '0') && (*pos <= '9'))
+  {
+    num[i++] = *pos++;
+  }
+  num[i] = '\0';
+  Result = atoi(num);
 
-    *String = ++pos;
+  *String = ++pos;
 
-    return Result;
+  return Result;
 }
 
 void
-ModifyLights(lightgrid *Grid, char* cmd)
+ModifyLights(lightgrid *Grid,
+             char* cmd)
 {
-    char* pos = cmd;
-    char cmdstring[10] = {};
-    operation Op = OP_NOP;
-    v2 StartLoc = {0,0};
-    v2 EndLoc = {0,0};
+  char* pos = cmd;
+  char cmdstring[10] = {};
+  operation Op = OP_NOP;
+  v2 StartLoc = {0,0};
+  v2 EndLoc = {0,0};
 
 
-    for(int i=0; i<10; i++)
+  for(int i = 0; i<10; i++)
+  {
+    char l = *(pos++);
+    if(l == ' ')
     {
-        char l = *(pos++);
-        if(l == ' ')
-        {
-            if(strcmp(cmdstring, "toggle") == 0)
-                Op = OP_TOGGLE;
-            if(strcmp(cmdstring, "turn on") == 0)
-                Op = OP_ON;
-            if(strcmp(cmdstring, "turn off") == 0)
-                Op = OP_OFF;
+      if(strcmp(cmdstring, "toggle") == 0)
+        Op = OP_TOGGLE;
+      if(strcmp(cmdstring, "turn on") == 0)
+        Op = OP_ON;
+      if(strcmp(cmdstring, "turn off") == 0)
+        Op = OP_OFF;
 
-            if(Op != OP_NOP)
-                break;
-        }
-        cmdstring[i] = l;
+      if(Op != OP_NOP)
+        break;
     }
+    cmdstring[i] = l;
+  }
 
-    if(Op == OP_NOP)
-    {
-        ERROR("Didn't get an operation for %s! Aborting!\n", cmd);
-    }
-    else
-    {
-        StartLoc.X = GetCoord(&pos);
-        StartLoc.Y = GetCoord(&pos);
-        while(*pos++ != ' '){}
-        EndLoc.X = GetCoord(&pos);
-        EndLoc.Y = GetCoord(&pos);
-        
+  if(Op == OP_NOP)
+  {
+    ERROR("Didn't get an operation for %s! Aborting!\n", cmd);
+  }
+  else
+  {
+    StartLoc.X = GetCoord(&pos);
+    StartLoc.Y = GetCoord(&pos);
+    while(*pos++ != ' ') {}
+    EndLoc.X = GetCoord(&pos);
+    EndLoc.Y = GetCoord(&pos);
+
 //        INFO("%s", cmd);
 //        INFO("Operation: %d, From (%d, %d), To (%d, %d)\n", Op, StartLoc.X, StartLoc.Y, EndLoc.X, EndLoc.Y);
 
-        int ChangeCount = 0;
-        for(int Y = StartLoc.Y; Y <= EndLoc.Y; Y++)
+    int ChangeCount = 0;
+    for(int Y = StartLoc.Y; Y <= EndLoc.Y; Y++)
+    {
+      for(int X = StartLoc.X; X <= EndLoc.X; X++)
+      {
+        uint8* pos = Grid->Grid + (X + Y * Grid->Cols);
+        switch (Op)
         {
-            for(int X = StartLoc.X; X <= EndLoc.X; X++)
-            {
-                uint8* pos = Grid->Grid + (X + Y*Grid->Cols);
-                switch (Op)
-                {
-                    case(OP_TOGGLE):
-                        *pos ^= 1;
-                        break;
-                    case(OP_ON):
-                        *pos = 1;
-                        break;
-                    case(OP_OFF):
-                        *pos = 0;
-                        break;
-                    default:
-                        break;
-                }
-                ChangeCount++;
-            }
+        case (OP_TOGGLE):
+          *pos ^= 1;
+          break;
+
+        case (OP_ON):
+          *pos = 1;
+          break;
+
+        case (OP_OFF):
+          *pos = 0;
+          break;
+
+        default:
+          break;
         }
+        ChangeCount++;
+      }
+    }
 
 //        INFO("Changed %d Bulbs.\n", ChangeCount);
-    }
+  }
 
 //    printf("\n");
 }
@@ -190,21 +194,21 @@ ModifyLights(lightgrid *Grid, char* cmd)
     realize you mistranslated Santa's message from Ancient Nordic
     Elvish.
 
-The light grid you bought actually has individual brightness controls;
-each light can have a brightness of zero or more. The lights all start
-at zero.
+   The light grid you bought actually has individual brightness controls;
+   each light can have a brightness of zero or more. The lights all start
+   at zero.
 
-The phrase turn on actually means that you should increase the
-brightness of those lights by 1.
+   The phrase turn on actually means that you should increase the
+   brightness of those lights by 1.
 
-The phrase turn off actually means that you should decrease the
-brightness of those lights by 1, to a minimum of zero.
+   The phrase turn off actually means that you should decrease the
+   brightness of those lights by 1, to a minimum of zero.
 
-The phrase toggle actually means that you should increase the brightness of those lights by 2.
+   The phrase toggle actually means that you should increase the brightness of those lights by 2.
 
-What is the total brightness of all lights combined after following Santa's instructions?
+   What is the total brightness of all lights combined after following Santa's instructions?
 
-For example:
+   For example:
 
     turn on 0,0 through 0,0 would increase the total brightness by 1.
     toggle 0,0 through 999,999 would increase the total brightness by 2000000.
@@ -212,149 +216,154 @@ For example:
 
  **/
 void
-ModifyBrightness(lightgrid *Grid, char* cmd)
+ModifyBrightness(lightgrid *Grid,
+                 char* cmd)
 {
 
-    char* pos = cmd;
-    char cmdstring[10] = {};
-    operation Op = OP_NOP;
-    v2 StartLoc = {0,0};
-    v2 EndLoc = {0,0};
+  char* pos = cmd;
+  char cmdstring[10] = {};
+  operation Op = OP_NOP;
+  v2 StartLoc = {0,0};
+  v2 EndLoc = {0,0};
 
 
-    for(int i=0; i<10; i++)
+  for(int i = 0; i<10; i++)
+  {
+    char l = *(pos++);
+    if(l == ' ')
     {
-        char l = *(pos++);
-        if(l == ' ')
-        {
-            if(strcmp(cmdstring, "toggle") == 0)
-                Op = OP_TOGGLE;
-            if(strcmp(cmdstring, "turn on") == 0)
-                Op = OP_ON;
-            if(strcmp(cmdstring, "turn off") == 0)
-                Op = OP_OFF;
+      if(strcmp(cmdstring, "toggle") == 0)
+        Op = OP_TOGGLE;
+      if(strcmp(cmdstring, "turn on") == 0)
+        Op = OP_ON;
+      if(strcmp(cmdstring, "turn off") == 0)
+        Op = OP_OFF;
 
-            if(Op != OP_NOP)
-                break;
-        }
-        cmdstring[i] = l;
+      if(Op != OP_NOP)
+        break;
     }
+    cmdstring[i] = l;
+  }
 
-    if(Op == OP_NOP)
-    {
-        ERROR("Didn't get an operation for %s! Aborting!\n", cmd);
-    }
-    else
-    {
-        StartLoc.X = GetCoord(&pos);
-        StartLoc.Y = GetCoord(&pos);
-        while(*pos++ != ' '){}
-        EndLoc.X = GetCoord(&pos);
-        EndLoc.Y = GetCoord(&pos);
-        
-        // INFO("%s", cmd); 
-        // INFO("Operation: %d, From (%d, %d), To (%d, %d)", Op, StartLoc.X, StartLoc.Y, EndLoc.X, EndLoc.Y);
+  if(Op == OP_NOP)
+  {
+    ERROR("Didn't get an operation for %s! Aborting!\n", cmd);
+  }
+  else
+  {
+    StartLoc.X = GetCoord(&pos);
+    StartLoc.Y = GetCoord(&pos);
+    while(*pos++ != ' ') {}
+    EndLoc.X = GetCoord(&pos);
+    EndLoc.Y = GetCoord(&pos);
 
-        int ChangeCount = 0;
-        for(int Y = StartLoc.Y; Y <= EndLoc.Y; Y++)
+    // INFO("%s", cmd);
+    // INFO("Operation: %d, From (%d, %d), To (%d, %d)", Op, StartLoc.X, StartLoc.Y, EndLoc.X, EndLoc.Y);
+
+    int ChangeCount = 0;
+    for(int Y = StartLoc.Y; Y <= EndLoc.Y; Y++)
+    {
+      for(int X = StartLoc.X; X <= EndLoc.X; X++)
+      {
+        uint8* pos = Grid->Grid + (X + Y * Grid->Cols);
+        switch (Op)
         {
-            for(int X = StartLoc.X; X <= EndLoc.X; X++)
-            {
-                uint8* pos = Grid->Grid + (X + Y*Grid->Cols);
-                switch (Op)
-                {
-                    case(OP_TOGGLE):
-                        *pos += 2;
-                        break;
-                    case(OP_ON):
-                        *pos += 1;
-                        break;
-                    case(OP_OFF):
-                        if(*pos>0)
-                            *pos -= 1;
-                        break;
-                    default:
-                        break;
-                }
-                ChangeCount++;
-            }
+        case (OP_TOGGLE):
+          *pos += 2;
+          break;
+
+        case (OP_ON):
+          *pos += 1;
+          break;
+
+        case (OP_OFF):
+          if(*pos>0)
+            *pos -= 1;
+          break;
+
+        default:
+          break;
         }
+        ChangeCount++;
+      }
+    }
 
 //        INFO("Changed %d Bulbs.\n", ChangeCount);
-    }
+  }
 
 //    printf("\n");
 }
 
 void
-day6(memory_arena *Arena, bool32 Testing)
+day6(memory_arena *Arena,
+     bool32 Testing)
 {
-    lightgrid LightGridBoolean = {};
-    LightGridBoolean.Rows = 1000;
-    LightGridBoolean.Cols = 1000;
-    LightGridBoolean.Size = LightGridBoolean.Rows * LightGridBoolean.Cols * sizeof(int8);
+  lightgrid LightGridBoolean = {};
+  LightGridBoolean.Rows = 1000;
+  LightGridBoolean.Cols = 1000;
+  LightGridBoolean.Size = LightGridBoolean.Rows * LightGridBoolean.Cols * sizeof(int8);
 
-    LightGridBoolean.Grid = (uint8 *)PushSize(Arena, LightGridBoolean.Size);
-    InitializeGrid(&LightGridBoolean);
-    
-    lightgrid LightGridBrightness = {};
-    LightGridBrightness.Rows = 1000;
-    LightGridBrightness.Cols = 1000;
-    LightGridBrightness.Size = LightGridBrightness.Rows * LightGridBrightness.Cols * sizeof(int8);
+  LightGridBoolean.Grid = (uint8 *)PushSize(Arena, LightGridBoolean.Size);
+  InitializeGrid(&LightGridBoolean);
 
-    LightGridBrightness.Grid = (uint8 *)PushSize(Arena, LightGridBrightness.Size);
-    InitializeGrid(&LightGridBrightness);
+  lightgrid LightGridBrightness = {};
+  LightGridBrightness.Rows = 1000;
+  LightGridBrightness.Cols = 1000;
+  LightGridBrightness.Size = LightGridBrightness.Rows * LightGridBrightness.Cols * sizeof(int8);
 
-    char FileName[] = "files/day6.txt";
-    FILE *DAY6;
+  LightGridBrightness.Grid = (uint8 *)PushSize(Arena, LightGridBrightness.Size);
+  InitializeGrid(&LightGridBrightness);
 
-    DAY6 = fopen(FileName, "r");
-    if(DAY6)
+  char FileName[] = "files/day6.txt";
+  FILE *DAY6;
+
+  DAY6 = fopen(FileName, "r");
+  if(DAY6)
+  {
+    char fileline[100];
+    while(fgets(fileline, 100, DAY6))
     {
-        char fileline[100];
-        while(fgets(fileline, 100, DAY6))
-        {
-            ModifyLights(&LightGridBoolean, fileline);
-            ModifyBrightness(&LightGridBrightness, fileline);
-        }
-
-        fclose(DAY6);
-    }
-    else
-    {
-        printf("Unable to open file.\n");
+      ModifyLights(&LightGridBoolean, fileline);
+      ModifyBrightness(&LightGridBrightness, fileline);
     }
 
-    uint8 *pos = LightGridBrightness.Grid;
-    uint32 Lit = 0;
-    uint64 Brightness = 0;
-    while(pos < (LightGridBrightness.Grid+LightGridBrightness.Size))
-    {
-        if(*pos)
-        {
-            Lit++;
-            Brightness += *pos;
-        }
-        pos++;
-    }
+    fclose(DAY6);
+  }
+  else
+  {
+    printf("Unable to open file.\n");
+  }
 
-    uint32 BooleanLit = 0;
+  uint8 *pos = LightGridBrightness.Grid;
+  uint32 Lit = 0;
+  uint64 Brightness = 0;
+  while(pos < (LightGridBrightness.Grid + LightGridBrightness.Size))
+  {
+    if(*pos)
     {
-        uint8 *pos = LightGridBoolean.Grid;
-        BooleanLit = 0;
-        while(pos < (LightGridBoolean.Grid+LightGridBoolean.Size))
-        {
-            if(*pos)
-            {
-                BooleanLit++;
-            }
-            pos++;
-        }
+      Lit++;
+      Brightness += *pos;
     }
-    printf("Light data (p1):\n");
-    printf("There are %d lights on.\n", BooleanLit);
+    pos++;
+  }
 
-    printf("Light data (p2):\n");
-    printf("There are %d lights on.\n", Lit);
-    printf("Brightness is %llu.\n", Brightness);
+  uint32 BooleanLit = 0;
+  {
+    uint8 *pos = LightGridBoolean.Grid;
+    BooleanLit = 0;
+    while(pos < (LightGridBoolean.Grid + LightGridBoolean.Size))
+    {
+      if(*pos)
+      {
+        BooleanLit++;
+      }
+      pos++;
+    }
+  }
+  printf("Light data (p1):\n");
+  printf("There are %d lights on.\n", BooleanLit);
+
+  printf("Light data (p2):\n");
+  printf("There are %d lights on.\n", Lit);
+  printf("Brightness is %lu.\n", Brightness);
 }
--- src/day8.cpp
+++ src/day8.cpp
@@ -8,21 +8,21 @@
    ======================================================================== */
 
 /*
---- Day 8: Matchsticks ---
+   --- Day 8: Matchsticks ---
 
-Space on the sleigh is limited this year, and so Santa will be
-bringing his list as a digital copy. He needs to know how much space
-it will take up when stored.
+   Space on the sleigh is limited this year, and so Santa will be
+   bringing his list as a digital copy. He needs to know how much space
+   it will take up when stored.
 
-It is common in many programming languages to provide a way to escape
-special characters in strings. For example, C, JavaScript, Perl,
-Python, and even PHP handle special characters in very similar ways.
+   It is common in many programming languages to provide a way to escape
+   special characters in strings. For example, C, JavaScript, Perl,
+   Python, and even PHP handle special characters in very similar ways.
 
-However, it is important to realize the difference between the number
-of characters in the code representation of the string literal and the
-number of characters in the in-memory string itself.
+   However, it is important to realize the difference between the number
+   of characters in the code representation of the string literal and the
+   number of characters in the in-memory string itself.
 
-For example:
+   For example:
 
     "" is 2 characters of code (the two double quotes), but the string
     contains zero characters.
@@ -35,125 +35,128 @@ For example:
     for a total of 7 characters in the string data.
 
     "\x27" is 6 characters of code, but the string itself contains
-    
 
-Santa's list is a file that contains many double-quoted string
-literals, one on each line. The only escape sequences used are \\
-(which represents a single backslash), \" (which represents a lone
-double-quote character), and \x plus two hexadecimal characters (which
-represents a single character with that ASCII code).
 
-Disregarding the whitespace in the file, what is the number of
-characters of code for string literals minus the number of characters
-in memory for the values of the strings in total for the entire file?
+   Santa's list is a file that contains many double-quoted string
+   literals, one on each line. The only escape sequences used are \\
+   (which represents a single backslash), \" (which represents a lone
+   double-quote character), and \x plus two hexadecimal characters (which
+   represents a single character with that ASCII code).
 
-For example, given the four strings above, the total number of
-characters of string code (2 + 5 + 10 + 6 = 23) minus the total number
-of characters in memory for string values (0 + 3 + 7 + 1 = 11) is 23 -
-11 = 12.
-)
-*/
+   Disregarding the whitespace in the file, what is the number of
+   characters of code for string literals minus the number of characters
+   in memory for the values of the strings in total for the entire file?
+
+   For example, given the four strings above, the total number of
+   characters of string code (2 + 5 + 10 + 6 = 23) minus the total number
+   of characters in memory for string values (0 + 3 + 7 + 1 = 11) is 23 -
+   11 = 12.
+   )
+ */
 
 #include <helpers.h>
 
 struct charcounts
 {
-    uint32 CodeCount;
-    uint32 MemCount;
-    uint32 SubCount;
-    uint32 EncodedCount;
-    uint32 SubEncodedCount;
+  uint32 CodeCount;
+  uint32 MemCount;
+  uint32 SubCount;
+  uint32 EncodedCount;
+  uint32 SubEncodedCount;
 };
 
 charcounts
-CountChars(memory_arena *Arena, char *Filename)
+CountChars(memory_arena *Arena,
+           const char *Filename)
 {
-    filelines *Day8 = GetFile(Arena, Filename);
+  filelines *Day8 = GetFile(Arena, Filename);
 
-    uint32 codecount = 0;
-    uint32 memcount = 0;
-    uint32 encodedcount = 0;
-    for(int32 l = 0; l < Day8->NumLines; ++l)
+  uint32 codecount = 0;
+  uint32 memcount = 0;
+  uint32 encodedcount = 0;
+  for(int32 l = 0; l < Day8->NumLines; ++l)
+  {
+    uint8 *ch = (uint8 *)Day8->Lines[l];
+    uint32 inner_codecount = 0;
+    uint32 inner_memcount = 0;
+    uint32 inner_enccount = 2;     // 2 represents the added '"' at each end.
+    while(*ch)
     {
-        uint8 *ch = (uint8 *)Day8->Lines[l];
-        uint32 inner_codecount = 0;
-        uint32 inner_memcount = 0;
-        uint32 inner_enccount = 2; // 2 represents the added '"' at each end.
-        while(*ch)
+      switch (*ch)
+      {
+      case '"':
+      {
+        inner_enccount += 2;
+      } break;
+
+      case '\\':
+      {
+        inner_enccount += 2;
+        if(*(ch + 1) == '\\')
         {
-            switch (*ch)
-            {
-                case '"':
-                {
-                    inner_enccount += 2;
-                }break;
-
-                case '\\':
-                {
-                    inner_enccount += 2;
-                    if(*(ch+1) == '\\')
-                    {
-                        inner_codecount++;
-                        ch++;
-                        inner_memcount++;
-                        inner_enccount += 2;
-                    }
-                    else if(*(ch+1) == 'x')
-                    {
-                        inner_codecount += 3;
-                        inner_memcount++;
-                        ch += 3;
-                        inner_enccount += 3;
-                    }
-                    else if(*(ch+1) == '"')
-                    {
-                        inner_codecount++;
-                        ch++;
-                        inner_memcount++;
-                        inner_enccount += 2;
-                    }
-                }break;
-                default:
-                {
-                    inner_memcount++;
-                    inner_enccount++;
-                }break;
-            }
-            ch++;
-            inner_codecount++;
+          inner_codecount++;
+          ch++;
+          inner_memcount++;
+          inner_enccount += 2;
         }
+        else if(*(ch + 1) == 'x')
+        {
+          inner_codecount += 3;
+          inner_memcount++;
+          ch += 3;
+          inner_enccount += 3;
+        }
+        else if(*(ch + 1) == '"')
+        {
+          inner_codecount++;
+          ch++;
+          inner_memcount++;
+          inner_enccount += 2;
+        }
+      } break;
 
-        codecount += inner_codecount;
-        memcount += inner_memcount;
-        encodedcount += inner_enccount;
+      default:
+      {
+        inner_memcount++;
+        inner_enccount++;
+      } break;
+      }
+      ch++;
+      inner_codecount++;
     }
 
-    charcounts Counts = {};
-    Counts.CodeCount = codecount;
-    Counts.MemCount = memcount;
-    Counts.SubCount = (codecount - memcount);
-    Counts.EncodedCount = encodedcount;
-    Counts.SubEncodedCount = encodedcount - codecount;
+    codecount += inner_codecount;
+    memcount += inner_memcount;
+    encodedcount += inner_enccount;
+  }
 
-    return(Counts);
+  charcounts Counts = {};
+  Counts.CodeCount = codecount;
+  Counts.MemCount = memcount;
+  Counts.SubCount = (codecount - memcount);
+  Counts.EncodedCount = encodedcount;
+  Counts.SubEncodedCount = encodedcount - codecount;
+
+  return(Counts);
 }
 
 
 void
-day8(memory_arena *Arena, bool32 Testing)
+day8(memory_arena *Arena,
+     bool32 Testing)
 {
 
-    if(Testing)
-    {
-        charcounts Counts = CountChars(Arena, "files/Day8_test1.txt");
-        printf("Testing: Code: %d, Memory: %d, Subtraction: %d\n", Counts.CodeCount, Counts.MemCount, Counts.SubCount);
-        printf("  Part2: New Encoding: %d, New Sub: %d\n", Counts.EncodedCount, Counts.SubEncodedCount);
-    }
+  if(Testing)
+  {
+    charcounts Counts = CountChars(Arena, "files/day8_test1.txt");
+    printf("Testing: Code: %d, Memory: %d, Subtraction: %d\n", Counts.CodeCount, Counts.MemCount, Counts.SubCount);
+    printf("  Part2: New Encoding: %d, New Sub: %d\n", Counts.EncodedCount, Counts.SubEncodedCount);
+  }
 
-    {
-        charcounts Counts = CountChars(Arena, "files/Day8.txt");
-        printf("Part 1: Code: %d, Memory: %d, Subtraction: %d\n", Counts.CodeCount, Counts.MemCount, Counts.SubCount);
-        printf("Part 2: New Encoding: %d, New Sub: %d\n", Counts.EncodedCount, Counts.SubEncodedCount);
-    }
+  {
+    charcounts Counts = CountChars(Arena, "files/day8.txt");
+    printf("Part 1: Code: %d, Memory: %d, Subtraction: %d\n", Counts.CodeCount, Counts.MemCount, Counts.SubCount);
+    printf("Part 2: New Encoding: %d, New Sub: %d\n", Counts.EncodedCount, Counts.SubEncodedCount);
+  }
 
 }
